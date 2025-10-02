750ad3ceb23369ddcfdb01e0e6059ba402e49869
Inkesk-Dozing <inkeskdozing@gmail.com>
2025-10-02 22:51:08 +0530

    OCR and Parsing Functions Added

---
+++ main.py
@@ -0,0 +1,26 @@
+import pytesseract
+
+
+def OCR(image_path):
+    '''Extraction of text from image using OCR i.e. by pytesseract library.'''
+    try:
+        text = pytesseract.image_to_string(Image.open(image_path))
+        return text
+    except Exception as e:
+        print(f"Could not process image {image_path}: {e}")
+        return ""
+
+
+def parse_and_group(text, data_dict):
+    '''Parsing of the text and assigining a group heading, updation of data dict.
+    Looks for headings with ':' and groups by the first part'''
+    lines = text.split('\n')
+    for line in lines:
+        if ':' in line:
+            heading, content = line.split(':', 1)
+            heading = heading.strip()
+            content = content.strip()
+            if heading not in data_dict:
+                data_dict[heading] = []
+            data_dict[heading].append(content)
+    return data_dict
